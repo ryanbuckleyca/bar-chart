14f2257669744899159f44f3d1144b60f8c2f286
Ryan Buckley <ryanbuckley@gmail.com>
2020-05-05 23:26:44 +0000

    Enabled stacked values. Modified HTML to account for two value sets. Adjusted JS to accommodate additional arrays.

--- drawBarChart.js
+++ drawBarChart.js
@@ -1,19 +1,6 @@
 // Bar Chart API
 
-//data = the data the chart should work from
-//options = an object which has options for the chart
-//element = a DOM element or jQuery element that the chart will render into
-function drawBarChart(data, options, element) {
-
-  //data is an array of arrays:
-  //[1, 2, 3, 4, 5], ['one', 'two', 'three', 'four', 'five']
-  let xValues = data[0]; //values
-  let xLabels = data[1]; //labels
-
-  //determine max value, then create multiplier to scale
-  let chartHeight = Math.max(...xValues);
-  let heightXer = window.innerHeight / chartHeight * .8;
-
+function createHTML(options, element) {
   let node = document.createElement("header");
   node.id = "chartTitle";
   node.style.color = options["titleFontColour"];
@@ -48,7 +35,6 @@ function drawBarChart(data, options, element) {
   node.innerHTML = "<p style=\"margin: 0;\">&nbsp;</p>";
   document.getElementById("y-axis").appendChild(node);
 
-
   node = document.createElement("figure");
   node.id = "barChart";
   node.style.display = "flex";
@@ -59,69 +45,107 @@ function drawBarChart(data, options, element) {
   node.style.alignItems = "flex-end";
   document.getElementById("chartSection").appendChild(node);
 
-  let barTxtPos;
-  if(options["barPosition"] === "top") {
-    barTxtPos = "flex-start";
-  } else if(options["barPosition"] === "bottom") {
-    barTxtPos = "flex-end";
-  } else {
-    barTxtPos = "center";
-  }
+}
 
-  for(let i = 0; i < xValues.length; i++) {
-    let barHeight = data[0][i] * heightXer;
+//data = the data the chart should work from
+//options = an object which has options for the chart
+//element = a DOM element or jQuery element that the chart will render into
+function drawBarChart(data, options, element) {
 
-    // Create a CONTAINER COLUMN
-    // This way there could be multiple bars in a column
-    node = document.createElement("div");
-    node.id = "bar" + i + "container";
-    node.style.display = "flex";
-    node.style.flexDirection = "column-reverse";
-    node.style.flex = 1;
-    node.style.margin = 0;
-    node.style.padding = 0;
-    document.getElementById("barChart").appendChild(node);
+  //create the HTML elements into which the graph data will go
+  createHTML(options, element);
 
-    //add Y ticks
-    node = document.createElement("div");
-    node.id = "y" + (i + 1);
-    node.style.display = "flex";
-    node.style.flexGrow = 1;
-    node.innerHTML = "<p style=\"margin: 0;\">" + Math.round(chartHeight / (xValues.length+1) * (i+1)) + " &mdash; </p>";
-    document.getElementById("y-axis").appendChild(node);
+  let maxValue = 0;
+  let labelColor;
+  let barColor;
+  //for each Chart in Stack:
+  for(let j = 0; j < data.length; j += 2) {
 
+    //data is an array of arrays
+    //first set is values, followed by labels
+    //i.e. [1, 2, 3, 4, 5], ['one', 'two', 'three', 'four', 'five']
+    let xValues = data[j]; //values
+    let xLabels = data[j+1]; //labels
 
-    //add value to X-Axis
-    node = document.createElement("aside");
-    node.id = "bar" + i + "label";
-    node.style.display = "flex";
-    node.style.flexDirection = "row";
-    node.style.marginRight = options["barSpacing"];
-    node.style.justifyContent = "space-evenly";
-    node.innerHTML = xLabels[i];
-    document.getElementById("bar" + i + "container").appendChild(node);
+    //determine the chart's max value, then create multiplier to scale
+    let chartHeight = Math.max(...xValues);
+    maxValue += chartHeight;
+    let heightXer = window.innerHeight / chartHeight * .8;
 
-  //LOOP for each stack in multiple-values chart
-    // Create each BAR inside
-    node = document.createElement("div");
-    node.id = "bar" + i;
-    node.style.display = "flex";
-    node.style.flexDirection = "row";
-    node.style.justifyContent = "space-evenly";
-    node.style.alignItems = barTxtPos;
-    node.style.background = options["barColor"];
-    node.style.height = barHeight + "px";
-    node.style.marginRight = options["barSpacing"];
-    node.style.color = options["labelColor"];
-    document.getElementById("bar" + i + "container").appendChild(node);
+    let barTxtPos;
+    if(options["barPosition"] === "top") {
+      barTxtPos = "flex-start";
+    } else if(options["barPosition"] === "bottom") {
+      barTxtPos = "flex-end";
+    } else {
+      barTxtPos = "center";
+    }
 
-    // Create TEXT
-    node = document.createElement("p");
-    node.style.margin = "0px";
-    node.innerHTML = xValues[i];
-    document.getElementById("bar" + i).appendChild(node);
-  //END LOOP
+    //for each X-Value, do something:
+    for(let i = 0; i < xValues.length; i++) {
+      let barHeight = data[j][i] * heightXer;
 
+      if(j === 0) {
+        // Create a CONTAINER COLUMN
+        // This way there could be multiple bars in a column
+        node = document.createElement("div");
+        node.id = "bar" + i + "container";
+        node.style.display = "flex";
+        node.style.flexDirection = "column-reverse";
+        node.style.flex = 1;
+        node.style.margin = 0;
+        node.style.padding = 0;
+        document.getElementById("barChart").appendChild(node);
+
+        //Create LABEL ELEMENT in X-Axis
+        node = document.createElement("aside");
+        node.id = "bar" + i + "label";
+        node.style.display = "flex";
+        node.style.flexDirection = "column-reverse";
+        node.style.marginRight = options["barSpacing"];
+        node.style.justifyContent = "space-evenly";
+        node.style.alignItems = "center";
+        document.getElementById("bar" + i + "container").appendChild(node);
+      }
+
+      //on the first go-round, add Y ticks
+      if(j === (data.length - 2)) {
+        node = document.createElement("div");
+        node.id = "y" + (i + 1);
+        node.style.display = "flex";
+        node.style.flexGrow = 1;
+        node.innerHTML = "<p style=\"margin: 0;\">" + Math.round(maxValue / (xValues.length+1) * (i+1)) + " &mdash; </p>";
+        document.getElementById("y-axis").appendChild(node);
+      }
+
+      Array.isArray(options["barColor"]) ? barColor = options["barColor"][j/2] : barColor = options["barColor"];
+      Array.isArray(options["labelColor"]) ? labelColor = options["labelColor"][j/2] : labelColor = options["labelColor"];
+
+      // Create each BAR inside
+      node = document.createElement("div");
+      node.id = "bar" + i + j;
+      node.style.display = "flex";
+      node.style.flexDirection = "row";
+      node.style.justifyContent = "space-evenly";
+      node.style.alignItems = barTxtPos;
+      node.style.background = barColor;
+      node.style.height = barHeight + "px";
+      node.style.marginRight = options["barSpacing"];
+      node.style.color = labelColor;
+      document.getElementById("bar" + i + "container").appendChild(node);
+
+      // Create TEXT inside of BAR
+      node = document.createElement("p");
+      node.style.margin = "0px";
+      node.innerHTML = xValues[i];
+      document.getElementById("bar" + i + j).appendChild(node);
+
+      // Create TEXT inside of X-label
+      node = document.createElement("p");
+      node.style.margin = "0px";
+      node.innerHTML = xLabels[i];
+      document.getElementById("bar" + i + "label").appendChild(node);
+    }
   }
 
   //OPTIONS
